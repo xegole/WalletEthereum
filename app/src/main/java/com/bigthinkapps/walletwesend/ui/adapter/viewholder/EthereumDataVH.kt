4db82c5841ca3a822b47197ca4acb19303ca8dd3
Diego Leon <didoleon3011@gmail.com>
2019-01-08 18:54:22 -0500

    add logic for view holder
    Signed-off-by: Diego Leon <didoleon3011@gmail.com>

--- app/src/main/java/com/bigthinkapps/walletwesend/ui/adapter/viewholder/EthereumDataVH.kt
+++ app/src/main/java/com/bigthinkapps/walletwesend/ui/adapter/viewholder/EthereumDataVH.kt
@@ -2,11 +2,18 @@ package com.bigthinkapps.walletwesend.ui.adapter.viewholder
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.bigthinkapps.walletwesend.R
 import com.bigthinkapps.walletwesend.model.EthereumData
+import kotlinx.android.synthetic.main.item_ethereum_data.view.*
+import java.util.*
 
 class EthereumDataVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun setData(ethereumData: EthereumData) {
+        val labelAddress = String.format(Locale.getDefault(), itemView.context.getString(R.string.label_ethereum_address), ethereumData.address)
+        val labelBalance = String.format(Locale.getDefault(), itemView.context.getString(R.string.label_ethereum_balance), ethereumData.balance)
 
+        itemView.labelEthereumAddress.text = labelAddress
+        itemView.labelEthereumBalance.text = labelBalance
     }
 }
